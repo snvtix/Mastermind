a9fabba5e889c96e5c0d3c1e5802da3f057d726b
agoscini <01169825@pw.edu.pl>
2022-05-21 13:27:11 +0200

    dodałem komentarze co jest to i dodatkowow zakomentowana jest alternatywa dodawania do wyników zgadnięcia lub prawie zgadnięcia, tyle że bez podawania konkretnej pozycji

--- src/pl/edu/pw/elka/prm2t/project/mastermind/MastermindExample.java
+++ src/pl/edu/pw/elka/prm2t/project/mastermind/MastermindExample.java
@@ -7,23 +7,27 @@ public class MastermindExample {
 
     public static void main(String[]args)
     {
-        Random r = new Random();
-        Scanner sc = new Scanner(System.in);
-        int[] haslo = new int[4];
-        for (int i = 0; i <4; i++) {
+        Random r = new Random(); // generator losowych liczb
+
+        Scanner sc = new Scanner(System.in); //input zwykły
+
+        int[] haslo = new int[4]; //haslo do zgadnięcia
+
+
+        for (int i = 0; i <4; i++){   //losowanie hasla
             haslo[i] = r.nextInt(9)+1 ;
         }
 
         int[] zgadniecia = new int[4];
 
-        ArrayList<String> wyniki = new ArrayList<String>();
+        ArrayList<String> wyniki = new ArrayList<String>(); // no jakby arraylist z wynikami poprzednimi
         boolean winner = false;
 
 
         System.out.println("Jakies intrukcje czy cos, zgaduj, liczby od 1 do 9");
 
 
-        int licznikZgadnięć = 0;
+        int licznikZgadnięć = 0; // licznik zganięć
 
         while (licznikZgadnięć<10) {
             licznikZgadnięć++;
@@ -32,15 +36,23 @@ public class MastermindExample {
 
             System.out.println("Dawaj strzelaj, to już " + licznikZgadnięć + " próba, a masz 10 jak coś");
 
-            for(int i =0; i<4; i++) {
+            for(int i =0; i<4; i++){   //ten input do odpowiedzi wrzucany tutaj
                 System.out.println("Pozycja " + pos++ + "");
                 zgadniecia[i] = sc.nextInt();
 
-                if (zgadniecia[i] > 9 || zgadniecia [i] < 1){  
+                if (zgadniecia[i] > 9 || zgadniecia [i] < 1){  // ustawienie granic liczb do wpisania
                     throw new IllegalArgumentException("Gościu między 1 a 9 strzelamy, czaisz?");
                 }
             }
+            //jak dobra cyfra na dobrej pozycji to dodajesz git, opcja która nie pokazuje na której pozycji trafione
+            /*for (int i=0; i<4; i++) {
+                if (zgadniecia[i] == haslo[i]) {
+                    wyniki.add("git");
+                }
 
+            }*/
+
+            //opcja która pokazuje na której pozycji trafione
             if (zgadniecia[0]==haslo[0]) {
                 wyniki.add("git1");
             }
@@ -58,7 +70,7 @@ public class MastermindExample {
             }
 
 
-
+            // jeśli cyfra pojawia się w hasle to dodaje prawie git
             if (zgadniecia[0]==haslo[1] || zgadniecia[0]==haslo[2] || zgadniecia[0]==haslo[3]) {
                 wyniki.add("prawie git1");
             }
@@ -76,16 +88,39 @@ public class MastermindExample {
             }
 
 
+            //opcja która nie pokazuje na której pozycji prawie trafione
+           /* if (zgadniecia[0]==haslo[1] || zgadniecia[0]==haslo[2] || zgadniecia[0]==haslo[3]) {
+                wyniki.add("prawie git");
+            }
+
+            if (zgadniecia[1]==haslo[2] || zgadniecia[1]==haslo[3] || zgadniecia[1] == haslo[0]) {
+                wyniki.add("prawie git");
+            }
+
+            if (zgadniecia[2]==haslo[0] || zgadniecia[2]==haslo[1] || zgadniecia[2] == haslo[3]) {
+                wyniki.add("prawie git");
+            }
+
+            if (zgadniecia[3]==haslo[0] || zgadniecia[3]==haslo[1] || zgadniecia[3] == haslo[2]) {
+                wyniki.add("prawie git");
+            }*/
+
+
+
+            // wychodzi z pętli jeśli znajdzie haslo
             if (zgadniecia[0] == haslo[0] && zgadniecia[1] == haslo[1] && zgadniecia[2] == haslo[2] && zgadniecia[3] == haslo[3]) {
                 winner = true;
                 break;
             }
 
-
+            // tu jeszcze muszę napisać coś co będzie wstawiało 0 w nietrafione miejsca
+            // no i zrobić tak żeby pokazywało po kolei trafione prawie trafione i nietrafione sloty
+            // żeby było od razu np [0, git, 0, prawie git]
+            // musi sprawdzać po kolei każdą wpisaną wartość a nie wszystkie na raz względem jednej kategorii
             System.out.print("Obczaj sobie jak ci idzie " + wyniki);
 
 
-            wyniki.clear();
+            wyniki.clear(); // czyści liste żeby następne zgadnięcia tylko były wpisane bo inaczej sie burdel robi
 
             if (licznikZgadnięć == 10) {
                 System.out.println("przegrana");
@@ -94,8 +129,7 @@ public class MastermindExample {
 
         }
 
-
-        if (winner == true) {
+        if (winner) {
             System.out.println("wygrana");
         }
 
