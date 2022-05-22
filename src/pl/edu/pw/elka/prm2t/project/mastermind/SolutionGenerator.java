bf2adac304f810583167e03dfa7e9aaf18513347
Natalia <natalia@multimed.org>
2022-05-22 08:27:35 +0200

    hej

--- src/pl/edu/pw/elka/prm2t/project/mastermind/SolutionGenerator.java
+++ src/pl/edu/pw/elka/prm2t/project/mastermind/SolutionGenerator.java
@@ -3,13 +3,17 @@ package pl.edu.pw.elka.prm2t.project.mastermind;
 import java.util.Random;
 
 public class SolutionGenerator {
-    public static void main(String[]args)
-    {
+
+    public int[] haslo(){
         Random rand = new Random();
         int[] haslo = new int[5];
         for (int i = 0; i <5; i++){   //losowanie hasla
-            haslo[i] = rand.nextInt(9)+1 ;
+            haslo[i] = rand.nextInt(5);
         }
+        return haslo;
+    }
+
         //narazie robimy dla najprostrzych ustawien
         //kazda liczba potem bedzie odpowiadac odpowiedniemu kolorowi, co bedzie mialo zastosowanie w gui
+        //5 kolorów
 }
